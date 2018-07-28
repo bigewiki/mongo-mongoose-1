9c72cbde0d41c99907ab6f662c5d942b43331dab
Edward M <edwardtmuniz@gmail.com>
2018-07-28 14:16:26 -0700

    first commit

---
+++ test/test_helper.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/users_test', { useNewUrlParser: true });
+
+mongoose.connection
+  .once('open', () => console.log('Good to go!'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
