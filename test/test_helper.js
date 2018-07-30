c9de79fbcdf797fd4eed541e1bb32f78603d2e5b
Edward M <edwardtmuniz@gmail.com>
2018-07-29 19:18:25 -0700

    moved connection within a before hook

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,11 +3,15 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/users_test', { useNewUrlParser: true });
 
-mongoose.connection
-  .once('open', () => {})
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+before((done) => {
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
+
+
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
