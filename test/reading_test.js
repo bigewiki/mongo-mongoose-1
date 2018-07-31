2fc7b73051e7c626a5782274049cb3ba5377d5c7
Edward M <edwardtmuniz@gmail.com>
2018-07-30 20:22:34 -0700

    fixed broken done callback

--- test/reading_test.js
+++ test/reading_test.js
@@ -4,7 +4,7 @@ const User = require('../src/user');
 describe('Reading users out of the database', () => {
   let joe;
 
-  beforeEach(() => {
+  beforeEach((done) => {
     joe = new User({ name: 'Joe' })
     joe.save()
       .then(() => done());
